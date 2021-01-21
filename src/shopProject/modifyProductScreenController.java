645f1e53fb36c55e02564139478d2ec40de3f160
Michał Połeć <michal.polec1@gmail.com>
2021-01-21 14:30:03 +0100

    szlifv2

--- src/shopProject/modifyProductScreenController.java
+++ src/shopProject/modifyProductScreenController.java
@@ -75,6 +75,16 @@ public class modifyProductScreenController {
            }
        });
 
+       stockField.textProperty().addListener(new ChangeListener<String>() {
+           @Override
+           public void changed(ObservableValue<? extends String> observable, String oldValue,
+                               String newValue) {
+               if (!newValue.matches("\\d*")) {
+                   stockField.setText(newValue.replaceAll("[^\\d]", ""));
+               }
+           }
+       });
+
    }
 
     public void initializeData() throws SQLException, ClassNotFoundException {
