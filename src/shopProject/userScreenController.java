dec56050a3bf0d9419cc1fee3f12bed3c3926155
EwaKopec <ewa21019@gmail.com>
2021-01-28 22:45:40 +0100

    menu with JFoeniX - userScreen - images

--- src/shopProject/userScreenController.java
+++ src/shopProject/userScreenController.java
@@ -6,6 +6,8 @@ import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
@@ -17,6 +19,23 @@ public class userScreenController {
     public StackPane stackPaneFX;
     public Button productsButton;
     public Pane livingroomPane;
+    public Pane bedroomPane;
+    public Pane kitchenPane;
+    public Pane diningroomPane;
+    public Pane kidsroomPane;
+    public Pane homeofficePane;
+    public Pane bathroomPane;
+    public Pane hallPane;
+
+
+    public ImageView livingroomImage;
+    public ImageView bedroomImage;
+    public ImageView diningImage;
+    public ImageView kitchenImage;
+    public ImageView kidsImage;
+    public ImageView officeImage;
+    public ImageView bathroomImage;
+    public ImageView hallImage;
 
 
     public void getAllData(ObservableList<Product> products) {
@@ -25,8 +44,8 @@ public class userScreenController {
 
     public void initialize()
     {
-        drawerAction();
         initializeDrawer();
+        drawerAction();
     }
 
     private void drawerAction()
@@ -51,9 +70,17 @@ public class userScreenController {
     {
         drawerFX.close();
         drawerFX.setSidePane(stackPaneFX);
-        drawerFX.setDefaultDrawerSize(300);
+        drawerFX.setDefaultDrawerSize(280);
         drawerFX.setOverLayVisible(true);
         drawerFX.setResizableOnDrag(true);
+        livingroomImage.setImage(new Image("images/living.png"));
+        bedroomImage.setImage(new Image("images/bedroom.png"));
+        diningImage.setImage(new Image("images/diningroom.png"));
+        kitchenImage.setImage(new Image("images/kitchen.png"));
+        kidsImage.setImage(new Image("images/kidsroom.png"));
+        officeImage.setImage(new Image("images/homeoffice.png"));
+        bathroomImage.setImage(new Image("images/bathroom.png"));
+        hallImage.setImage(new Image("images/hall.png"));
     }
 
 
